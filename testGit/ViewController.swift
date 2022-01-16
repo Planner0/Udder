6f9f145b783d8fcb4326e3e3376678241ae7d418
Aleksandr Pozdnikin <chishirik0@gmail.com>
2022-01-16 16:00:22 +0300

    Changes ViewController.

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
     }
+    func git() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
 
 
 }
